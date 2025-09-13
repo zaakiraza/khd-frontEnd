944b542ecc45534c2842c52913d2f12c75f19298
zaakiraza <raaza.zaakir@gmail.com>
2025-09-13 18:41:24 +0500

    updates

--- src/Components/Pages/Login/LoginUser.jsx
+++ src/Components/Pages/Login/LoginUser.jsx
@@ -1,25 +1,63 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./LoginUser.css";
+import axios from "axios";
 
-function LoginUser() {
-  const handleSubmit = (e) => {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const BASEURL = import.meta.env.VITE_BASEURL;
+  const logo = "/logo.png";
+
+  const formHandler = async (e) => {
     e.preventDefault();
-    console.log("form submitted");
+    try {
+      const api = await axios.post(`${BASEURL}/auth/admin_login`, {
+        email,
+        password,
+      });
+      setMessage(api.data.message);
+      if (api.data.status) {
+        localStorage.setItem("token", api.data.data);
+        navigate("/dashboard/student");
+      }
+
+      setMessage(api.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
   };
 
   return (
-    <div className="box">
-      <div className="content-login">
-        <h1>User Login</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Login</button>
+    <div className="main">
+      <div className="form_box">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <h1>Khuddam User Login</h1>
+        </div>
+        <form className="form_user">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* <button className="btnLogin" onClick={formHandler}> */}
+          <button className="btnLogin">Login</button>
         </form>
+        <Link className="lst_anker" to={"/"}>
+          Back To Home
+        </Link>
+        <p>{message}</p>
       </div>
     </div>
   );
 }
 
-export default LoginUser;
+export default Login;
